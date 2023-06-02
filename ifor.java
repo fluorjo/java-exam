f65f8b0173ce1f4a4fb0b870e57fba67a1839eb7
fluorjo <fluorjo@naver.com>
2023-06-02 09:43:31 +0900

    if문에 or 있을 경우

---
+++ ifor.java
@@ -0,0 +1,10 @@
+public class ifor {
+    public static void main(String[] args)
+{
+    int num1 = 3;
+    int num2 = 7;
+    if(++num1 < 5 || ++num2 > 8)
+        System.out.println(num1);
+    System.out.println(num2);
+}
+}
